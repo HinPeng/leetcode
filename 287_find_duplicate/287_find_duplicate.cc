e6cbaba7987bdbe7bc7c714c77b23a90b45cbe4e
Xuan Peng <pieceseix@gmail.com>
2018-08-07 00:13:17 +0800

    Add problem's description

--- 287_find_duplicate/287_find_duplicate.cc
+++ 287_find_duplicate/287_find_duplicate.cc
@@ -1,3 +1,21 @@
+/* Problem description
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
+prove that at least one duplicate number must exist. 
+Assume that there is only one duplicate number, find the duplicate one.
+
+Input: [1,3,4,2,2]
+Output: 2
+
+Note:
+  * You must not modify the array (assume the array is read only).
+  * You must use only constant, O(1) extra space.
+  * Your runtime complexity should be less than O(n2).
+  * There is only one duplicate number in the array, but it could be repeated more than once.
+
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <cassert>
